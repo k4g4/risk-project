1f690a58ccfc5734f7c7057992e26981a5ce3e55
Zgonz19 <gonzalo.zg.19@gmail.com>
2018-10-16 22:57:32 -0500

    maven project build

---
+++ src/main/java/ARG/Risk.java
@@ -0,0 +1,16 @@
+
+public class Risk {
+	
+	public static void main(String[] args) {
+		String title = "Risk";
+		String bg = "images/bg.jpg";
+		int width = 1280;
+		int height = 720;
+		Engine engine = new Engine("config/config.properties");
+  }
+	
+	//Shorthand for System.out.println
+	public static void print(Object printMe) {
+		System.out.println(printMe);
+	}
+}
